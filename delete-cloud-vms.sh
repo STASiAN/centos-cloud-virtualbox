4d967bd4b03f7f7535f6a766e699d84894c41176
Stanislav Shcherbakov <mail@stasian.ru>
2018-08-27 17:06:58 +0700

    fix dele cloud vm script

--- delete-cloud-vms.sh
+++ delete-cloud-vms.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+dir=~/VirtualBox\ VMs
+cd "$dir"
 images=$(ls -1 | grep vdi$ | sed 's/.vdi//g')
 for vm in $images; do
     if [ -f $vm.vdi ]; then
         VBoxManage controlvm $vm poweroff
         sleep 1
-        VBoxManage snapshot $vm-deployed delete
+        VBoxManage snapshot $vm delete $vm-deployed
         VBoxManage unregistervm $vm --delete
     fi
     rm -rf $vm $vm.vdi $vm.raw $vm-cidata.iso
